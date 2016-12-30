1179063ebbf2f4a4dda217092b8f35d37ed29b3a
xfwduke <xfwduke@gmail.com>
2016-12-30 16:50:17 +0800

    finish exercises

--- chapter-3.lisp
+++ chapter-3.lisp
@@ -60,3 +60,51 @@
 	 (if sp (cons begin sp))))))
 
 
+;; 3)
+(defun occurrences-helper (lst res)
+  (cond
+    ((null lst) res)
+    (t
+     (let ((obj (car lst)))
+       (if (assoc obj res)
+	   (setf
+	    (cdr (assoc obj res))
+	    (1+ (cdr (assoc obj res))))
+	   (push (cons obj 1) res))
+       (occurrences-helper (rest lst) res)))))
+
+(defun occurrences-r (lst)
+  (let ((res '((a . 0) (c . 0) (g . 0) (t . 0))))
+    (occurrences-helper lst res)))
+
+;; 5)
+(defun pos+ (lst)
+  (do ((idx 0 (incf idx)))
+      ((= idx (length lst)) lst)
+    (setf (nth idx lst) (+ idx (nth idx lst)))))
+
+;; 8)
+(defun showdots (lst)
+  (showdots-helper lst (length lst)))
+
+(defun showdots-helper (lst n)
+  (cond
+    ((null lst) (format t "~S" lst)
+     (dotimes (i n)
+       (format t ")")))
+    ((atom lst) (format t "(~S" lst))
+    (t
+     (showdots-helper (car lst) n)
+     (format t " . ")
+     (showdots-helper (cdr lst) n))))
+
+;; 答案的实现, 漂亮很多
+(defun showdots-1 (lst)
+  (format t "~A" (showdots-helper-1 lst)))
+
+(defun showdots-helper-1 (lst)
+  (if (atom lst)
+      lst
+      (format nil "(~A . ~A)"
+	      (showdots-helper-1 (car lst))
+	      (showdots-helper-1 (cdr lst)))))
