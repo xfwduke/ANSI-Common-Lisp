8726bab37f17c36c331917ae1c3a7b6e208392af
xfwduke <xfwduke@gmail.com>
2017-01-05 17:03:58 +0800

    finish partly chapter-4

--- chapter-4.lisp
+++ chapter-4.lisp
@@ -26,7 +26,8 @@
 	    (make-array  n))))
 
 (defstruct (node (:print-object
-		  (lambda (n s) (format s "#<~A>" (node-elt n)))))
+		  (lambda (n s)
+		    (format s "#<elt:~A>" (node-elt n)))))
   elt
   (left nil)
   (right nil))
@@ -41,6 +42,7 @@
 ;;; 而其他版本的二叉树生成算法是真的遍历已有的树
 ;;; 直到找到一个 left/right == nil 的合适节点时
 ;;; 用 obj 新建一个节点, 再添加上去
+
 (defun bst-insert (obj bst)
   (if (null bst)
       (make-node :elt obj)
@@ -57,3 +59,71 @@
 		 :left (node-left bst)
 		 :right (bst-insert obj (node-right bst))))))))
 
+
+(defun bst-insert-general (obj bst)
+  (if (null bst)
+      (make-node :elt obj)
+      (let ((elt (node-elt bst)))
+	(if (eql obj elt)
+	    bst
+	    (if (< obj elt)
+		(if (null (node-left bst))
+		    (setf (node-left bst) (make-node :elt obj))
+		    (bst-insert-general obj (node-left bst)))
+		(if (null (node-right bst))
+		    (setf (node-right bst) (make-node :elt obj))
+		    (bst-insert-general obj (node-right bst))))))))
+
+(defun bst-find (obj bst)
+  (cond
+    ((null bst) nil)
+    ((eql obj (node-elt bst)) bst)
+    ((< obj (node-elt bst)) (bst-find obj (node-left bst)))
+    (t (bst-find obj (node-right bst)))))
+
+(defun bst-min (bst)
+  (and bst
+       (or (bst-min (node-left bst)) bst)))
+
+(defun bst-min (bst)
+  (and bst
+       (or (bst-min (node-right bst)) bst)))
+
+(defun bst-tranverse (fn bst)
+  (cond
+    ((null bst) nil)
+    (t
+     (bst-tranverse fn (node-left bst))
+     (funcall fn bst)
+     (bst-tranverse fn (node-right bst)))))
+
+;;; 1)
+(defun quarter-turn (mt)
+  (let* ((dim (array-dimension mt 0))
+	 (res (make-array (list dim dim))))
+    (dotimes (m dim)
+      (dotimes (n dim)
+	(setf (aref res m n) (aref mt (- dim (+ n 1)) m))))
+   res))
+
+;;; 2)
+(defun my-copy-list (lst)
+  (reduce #'(lambda (a b)
+	      (append a (list b)))
+	  lst
+	  :initial-value nil))
+
+(defun my-reserve (lst)
+  (reduce #'(lambda (a b)
+	      (push b a))
+	  lst
+	  :initial-value nil))
+
+;;; 3)
+(defstruct (tri-node (:print-object
+		      (lambda (o s)
+			(format s "#<~A>" (tri-node-elt o)))))
+  elt
+  (left nil)
+  (middle nil)
+  (right nil))
